2e5c326e41a5aad4d4734519ff2979e2f6c5e7ec
Your name <omowunmidaud1@gmail.com>
2023-06-08 14:31:21 +0100

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,16 +7,31 @@ export default function Header() {
         <Link to={'/'}  className=''>
             <img src={pageLogo} alt="logo" />
         </Link>
+
         <ul className="flex gap-2 py-6 justify-evenly w-2/3">
-            <li className=' text-secondaryText'>Pregnancy</li>
-            <li className=' text-secondaryText'>Gynecologist</li>
-            <li className=' text-secondaryText'>Child Birth</li>
-            <li className=' text-secondaryText'>Baby Care</li>
-            <li className=' text-secondaryText'>Community</li>   
-            <Link to={'/'} className="">
+
+            <Link to={'/pregnancy'}  className=''>
+                <li className='text-secondaryText'>Pregnancy</li>
+            </Link>
+            <Link to={'/gyneacologist'}  className=''>
+                <li className='text-secondaryText'>Gynecologist</li>
+            </Link>
+            <Link to={'/'}  className=''>
+                <li className='text-secondaryText'>Child Birth</li>
+            </Link>
+            <Link to={'/'}  className=''>
+                <li className='text-secondaryText'>Baby Care</li>
+            </Link>
+            <Link to={'/'}  className=''>
+                <li className='text-secondaryText'>Community</li>
+            </Link>            
+               
+            <Link to={'/register'} className="">
               <button className="flex text-center rounded-lg bg-yellowgold px-6 py-1">Get Started</button>
             </Link>
+
         </ul>
+
       </header>
   )
 }
